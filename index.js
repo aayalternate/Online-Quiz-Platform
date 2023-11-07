f9cc6960092de87dbb30371ee002c3a00b0f48f8
A0D1I2L3 <adilhaneefa2005@gmail.com>
2023-11-07 12:58:11 +0530

    Added index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+var firebase = require('firebase');
+var firebaseui = require('firebaseui');
+
+
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+
