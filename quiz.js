78cc801a403f3498be9d4cd035106961e52fe48a
A0D1I2L3 <adilhaneefa2005@gmail.com>
2023-11-15 01:08:13 +0530

    answer checking

--- quiz.js
+++ quiz.js
@@ -1,32 +1,35 @@
 const questions=document.getElementsByClassName("container");
 q_count=0
+score=0
 for(let i=1;i<questions.length;i++){
     questions[i].style.display="none"
 }
 
+
 function renderQuestion(){
+var scoreDisplay=questions[q_count].querySelector("span");
+
+    scoreDisplay.innerHTML=score
+
     document.querySelectorAll('h1')[q_count].innerHTML=questionbank[q_count].question;
 
     allOptions=questions[q_count].getElementsByClassName("option")
     for(i=0;i<allOptions.length;i++){
         allOptions[i].innerHTML=questionbank[q_count].options[i];
-    }
+        allOptions[i].addEventListener("click",function(){
+            
+if(this.innerHTML==questionbank[q_count].correctAns)
+            {
+                score++
+                scoreDisplay.innerHTML=score
+                this.style.backgroundColor="green"
 
-    
-//     allOptions=questions[q_count].getElementsByClassName("option")
-//         for(i=0;i<allOptions.length;i++){
-//             allOptions[i].addEventListener("click",()=>{
-//                          if(questionbank[q_count].correctAns=allOptions[q_count].innerHTML){
-//                 //             score++
-//                 //             scoredisplay.innerHTML = score
-//                              allOptions[q_count].style.backgroundColor = "green";
-//                 //         }   
-//                                }
-// })
-//         }
+            }
+
+        })
+    }}
 
 
-    }
 
 document.getElementsByClassName("button-left")[0].addEventListener('click',()=>{
 
@@ -34,7 +37,7 @@ document.getElementsByClassName("button-left")[0].addEventListener('click',()=>{
         console.log(q_count)
 
 questions[q_count].style.display="none";
---q_count
+q_count--
 questions[q_count].style.display="grid";
 renderQuestion()
 
@@ -44,12 +47,11 @@ document.getElementsByClassName("button-right")[0].addEventListener('click',()=>
     if (q_count<questions.length-1){
         console.log(q_count)
     questions[q_count].style.display="none";
-    ++q_count
+    q_count++
     questions[q_count].style.display="grid";
     renderQuestion()
 
-}}
-    )
+}}  )
 
     let questionbank = [
         {
@@ -75,40 +77,3 @@ document.getElementsByClassName("button-right")[0].addEventListener('click',()=>
     ];
 
     renderQuestion()
-
-
-
-
-
-
-
-
-
-     // for(i=0;i<allOptions.length;i++){
-//     allOptions[i].addEventListener("click",()=>{
-//         if(questionbank[q_count].correctAns=this.value){
-//             score++
-//             scoredisplay.innerHTML = score
-//             document.getElementById('option2container').style.backgroundColor = "green"
-//         }
-        
-//     })
-//     allOptions[i].innerHTML=questionbank[q_count].options[i]
-
-// }
-
-
-
-
-
-
-
-
-        // score++
-        // scoredisplay.innerHTML = score
-
-        // document.getElementById('option1container').style.backgroundColor = "green"
-        
-
-    
-       
